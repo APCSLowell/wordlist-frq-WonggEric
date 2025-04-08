ea67ed77f726c1f36141dade337a2010d2520678
WonggEric <88013940+WonggEric@users.noreply.github.com>
2025-04-08 10:13:49 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -4,35 +4,29 @@ public class WordList
 {
   private ArrayList <String> myList;
 
-  public WordList() { 
-    myList = new ArrayList<String>();
-  }
-  public void add(String word) { 
-    myList.add(word);
-  }
-  public String toString() { 
-    return myList.toString();
-  }
+  public WordList() { myList = new ArrayList<String>(); }
+  public void add(String word) { myList.add(word); }
+  public String toString() { return myList.toString(); }
 
   public int numWordsOfLength(int len)
   {
+    /* to be implemented in part (a) */
     int count = 0;
-    for (int i = 0; i<myList.size(); i++) {
-      if (myList.get(i).length()==len){
+    for(int i = 0;i<myList.size();i++){
+      if(myList.get(i).length()==len){
         count++;
       }
-      return count;
-    }
+  }
+    return count;
   }
 
   public void removeWordsOfLength(int len)
   {
     /* to be implemented in part (b) */
-    for (int i = 0; i<myList.size(); i++) {
-      if (myList.get(i).length()!=len) {
-        myList.remove(myList.get(i));
+    for(int i = 0;i<myList.size();i++){
+      if(myList.get(i).length()==len){
+        myList.remove(i);
         i--;
       }
-    }
   }
 }
